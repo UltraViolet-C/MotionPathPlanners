0f1395b3fc959822b3ed253f273154c88e6150c5
YuvraajC <yuvraajchahil9@gmail.com>
2024-04-03 13:35:59 -0700

    Cleaned up code by removing imports and extracting state class for future use

--- src/rrt_planner.py
+++ src/rrt_planner.py
@@ -4,46 +4,9 @@ import pickle
 import numpy as np
 import random
 import cv2
-from matplotlib import pyplot as plt
-
-from itertools import product
-from math import sqrt
 
 from plotting_utils import draw_plan
-from priority_queue import priority_dict
-
-class State:
-    """
-    2D state.
-    """
-
-    def __init__(self, x, y, parent):
-        """
-        x represents the columns on the image and y represents the rows,
-        Both are presumed to be integers
-        """
-        self.x = x
-        self.y = y
-        self.parent = parent
-        self.children = []
-
-
-    def __eq__(self, state):
-        """
-        When are two states equal?
-        """
-        return state and self.x == state.x and self.y == state.y
-
-    def __hash__(self):
-        """
-        The hash function for this object. This is necessary to have when we
-        want to use State objects as keys in dictionaries
-        """
-        return hash((self.x, self.y))
-
-    def euclidean_distance(self, state):
-        assert (state)
-        return sqrt((state.x - self.x)**2 + (state.y - self.y)**2)
+from state import State
 
 class RRTPlanner:
     """
---
+++ src/state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from math import sqrt
+
+class State:
+    """
+    2D state.
+    """
+
+    def __init__(self, x, y, parent):
+        """
+        x represents the columns on the image and y represents the rows,
+        Both are presumed to be integers
+        """
+        self.x = x
+        self.y = y
+        self.parent = parent
+        self.children = []
+
+
+    def __eq__(self, state):
+        """
+        When are two states equal?
+        """
+        return state and self.x == state.x and self.y == state.y
+
+    def __hash__(self):
+        """
+        The hash function for this object. This is necessary to have when we
+        want to use State objects as keys in dictionaries
+        """
+        return hash((self.x, self.y))
+
+    def euclidean_distance(self, state):
+        assert (state)
+        return sqrt((state.x - self.x)**2 + (state.y - self.y)**2)
